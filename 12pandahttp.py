a61c5094066f1012702945e99cd5ce80a86aeb56
kirankolisetti <kiran.kolishetti@gmail.com>
2020-07-18 23:52:53 -0400

    read from panda

--- 12pandahttp.py
+++ 12pandahttp.py
@@ -1,8 +1,6 @@
 import requests
-from pandas.io.json 
-import json_normalize
 import pandas as pd
 
-url = "https://api.exchangerate-api.com/v4/latest/USD"
+url = requests.get('https://api.exchangerate-api.com/v4/latest/USD').text
 df = pd.read_json(url)
 print(df)
